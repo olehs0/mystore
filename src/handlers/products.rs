23da74f645e6cb37bac7f02b327ffc9021da65bf
oleh.samokhvalov <oleh.samokhvalov@anvileight.com>
2020-01-07 14:56:26 +0200

    INIT

---
+++ src/handlers/products.rs
@@ -0,0 +1,45 @@
+use actix_web::{HttpRequest, HttpResponse };
+
+use crate::models::product::ProductList;
+
+pub fn index(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().json(ProductList::list())
+}
+
+use crate::models::product::NewProduct;
+use actix_web::web;
+
+pub fn create(new_product: web::Json<NewProduct>) -> Result<HttpResponse, HttpResponse> {
+    new_product
+        .create()
+        .map(|product| HttpResponse::Ok().json(product))
+        .map_err(|e| {
+            HttpResponse::InternalServerError().json(e.to_string())
+        })
+}
+
+use crate::models::product::Product;
+
+pub fn show(id: web::Path<i32>) -> Result<HttpResponse, HttpResponse> {
+    Product::find(&id)
+        .map(|product| HttpResponse::Ok().json(product))
+        .map_err(|e| {
+            HttpResponse::InternalServerError().json(e.to_string())
+        })
+}
+
+pub fn destroy(id: web::Path<i32>) -> Result<HttpResponse, HttpResponse> {
+    Product::destroy(&id)
+        .map(|_| HttpResponse::Ok().json(()))
+        .map_err(|e| {
+            HttpResponse::InternalServerError().json(e.to_string())
+        })
+}
+
+pub fn update(id: web::Path<i32>, new_product: web::Json<NewProduct>) -> Result<HttpResponse, HttpResponse> {
+    Product::update(&id, &new_product)
+        .map(|_| HttpResponse::Ok().json(()))
+        .map_err(|e| {
+            HttpResponse::InternalServerError().json(e.to_string())
+        })
+}
---
+++ src/models/product.rs
@@ -0,0 +1,72 @@
+use crate::schema::products;
+
+#[derive(Serialize, Deserialize)]
+pub struct ProductList(pub Vec<Product>);
+
+#[derive(Queryable, Serialize, Deserialize)]
+pub struct Product {
+    pub id: i32,
+    pub name: String,
+    pub stock: f64,
+    pub price: Option<i32>
+}
+
+#[derive(Insertable, Deserialize, AsChangeset)]
+#[table_name="products"]
+pub struct NewProduct {
+    pub name: Option<String>,
+    pub stock: Option<f64>,
+    pub price: Option<i32>
+}
+
+use diesel::QueryDsl;
+use diesel::RunQueryDsl;
+use crate::schema::products::dsl;
+use crate::db_connection::establish_connection;
+
+impl ProductList {
+    pub fn list() -> Self {
+        let connection = establish_connection();
+
+        let result = 
+            products::table
+                .limit(10)
+                .load::<Product>(&connection)
+                .expect("Error loading products");
+
+        ProductList(result)
+    }
+}
+
+impl NewProduct {
+    pub fn create(&self) -> Result<Product, diesel::result::Error> {
+        let connection = establish_connection();
+        diesel::insert_into(products::table)
+            .values(self)
+            .get_result(&connection)
+    }
+}
+
+impl Product {
+    pub fn find(id: &i32) -> Result<Product, diesel::result::Error> {
+        let connection = establish_connection();
+
+        products::table.find(id).first(&connection)
+    }
+
+    pub fn destroy(id: &i32) -> Result<(), diesel::result::Error> {
+        let connection = establish_connection();
+
+        diesel::delete(dsl::products.find(id)).execute(&connection)?;
+        Ok(())
+    }
+
+    pub fn update(id: &i32, new_product: &NewProduct) -> Result<(), diesel::result::Error> {
+        let connection = establish_connection();
+
+        diesel::update(dsl::products.find(id))
+            .set(new_product)
+            .execute(&connection)?;
+        Ok(())
+    }
+}
