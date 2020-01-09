947e26356f9b263c53a9ff1b8ebcc475fbf32ed5
Danylo Lapirov <teggot@localhost.localdomain>
2020-01-09 23:58:12 +0200

    Add cors

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,29 @@
-pub mod schema;
 pub mod db_connection;
-pub mod models;
-pub mod handlers;
 pub mod errors;
+pub mod handlers;
+pub mod models;
+pub mod schema;
 pub mod utils;
 
 #[macro_use]
 extern crate diesel;
-extern crate dotenv;
-extern crate serde;
-extern crate serde_json;
-#[macro_use] 
+
+#[macro_use]
 extern crate serde_derive;
 
-extern crate actix;
-extern crate actix_cors;
-extern crate actix_web;
 extern crate bcrypt;
 extern crate jsonwebtoken as jwt;
 
 #[macro_use]
 extern crate dotenv_codegen;
 
-extern crate log;
-extern crate env_logger;
+use actix;
+use listenfd::ListenFd;
 
-extern crate actix_http;
-
-use actix_web::{App, HttpServer, web};
+use actix_cors::Cors;
 use actix_identity::{CookieIdentityPolicy, IdentityService};
-use actix_web::middleware::{cors, Logger};
+use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpServer};
 use chrono::Duration;
 use db_connection::establish_connection;
 
@@ -37,44 +31,47 @@ fn main() {
     std::env::set_var("RUST_LOG", "actix_web=debug");
     env_logger::init();
     let sys = actix::System::new("mystore");
+    let mut listenfd = ListenFd::from_env();
 
-    HttpServer::new(
-    move || App::new()
-        .wrap(Logger::default())
-        .wrap(
-            IdentityService::new(
+    let mut server = HttpServer::new(move || {
+        App::new()
+            .wrap(Cors::new().supports_credentials())
+            .wrap(Logger::default())
+            .wrap(IdentityService::new(
                 CookieIdentityPolicy::new(dotenv!("SECRET_KEY").as_bytes())
                     .domain(dotenv!("HOST"))
                     .name("jwt")
                     .path("/")
                     .max_age(Duration::days(1).num_seconds())
-                    .secure(dotenv!("COOKIE_SECURE").parse().unwrap())
+                    .secure(dotenv!("COOKIE_SECURE").parse().unwrap()),
+            ))
+            .data(establish_connection())
+            .service(
+                web::resource("/products")
+                    .route(web::get().to(handlers::products::index))
+                    .route(web::post().to(handlers::products::create)),
             )
-        )
-        .data(establish_connection())
-        .service(
-            web::resource("/products")
-                .route(web::get().to(handlers::products::index))
-                .route(web::post().to(handlers::products::create))
-        )
-        .service(
-            web::resource("/products/{id}")
-                .route(web::get().to(handlers::products::show))
-                .route(web::delete().to(handlers::products::destroy))
-                .route(web::patch().to(handlers::products::update))
-        )
-        .service(
-            web::resource("/register")
-                .route(web::post().to(handlers::register::register))
-        )
-        .service(
-            web::resource("/auth")
-                .route(web::post().to(handlers::authentication::login))
-                .route(web::delete().to(handlers::authentication::logout))
-        )
-    )
-    .bind("0.0.0.0:8088").unwrap()
-    .start();
+            .service(
+                web::resource("/products/{id}")
+                    .route(web::get().to(handlers::products::show))
+                    .route(web::delete().to(handlers::products::destroy))
+                    .route(web::patch().to(handlers::products::update)),
+            )
+            .service(web::resource("/register").route(web::post().to(handlers::register::register)))
+            .service(
+                web::resource("/auth")
+                    .route(web::post().to(handlers::authentication::login))
+                    .route(web::delete().to(handlers::authentication::logout)),
+            )
+    });
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l).unwrap()
+    } else {
+        server.bind("127.0.0.1:3000").unwrap()
+    };
+
+    server.start();
 
     println!("Started http server: 0.0.0.0:8088");
     let _ = sys.run();
