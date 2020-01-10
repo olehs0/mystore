93186640c8c297678cb36d1579f5be040cfc6691
Oleh <thebigbangfirst@gmail.com>
2020-01-10 21:26:06 +0200

    add 0.0.0.0 bind

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,7 @@ fn main() {
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l).unwrap()
     } else {
-        server.bind("127.0.0.1:8088").unwrap()
+        server.bind("0.0.0.0:8088").unwrap()
     };
 
     server.start();
